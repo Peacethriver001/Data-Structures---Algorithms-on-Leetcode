06c9d843d5c6aab914ffa2e8d3f2b85ed917b2ab
Peace Bakare <Peace.Bakare@sterling.ng>
2022-04-01 10:41:16 +0100

    Add Two Numbers question on Leetcode

--- Solutions/Day-01/AddTwoNumbers.java
+++ Solutions/Day-01/AddTwoNumbers.java
@@ -3,23 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Day_01;
-
 /**
  *
  * @author Peace E. BAKARE
  */
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+
 public class AddTwoNumbers {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummyNode = new ListNode(0);
