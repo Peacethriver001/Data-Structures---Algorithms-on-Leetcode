3f4b785e8433e8fe7c2b21a42932232552d68195
Peace Bakare <Peace.Bakare@sterling.ng>
2022-04-01 18:04:13 +0100

    Finding the maximum depth using Bottom Up approach of Tree Traversal

---
+++ Solutions/Day_02/BottomUpTreeRecursion.java
@@ -0,0 +1,11 @@
+public class BottomUpTreeRecurssion {
+    public int maximum_depth(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+
+        int left_depth = maximum_depth(root.left);
+        int right_depth = maximum_depth(root.right);
+        return Math.max(left_depth, right_depth) + 1;
+    }
+}
